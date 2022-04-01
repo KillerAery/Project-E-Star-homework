045e5333e1ecc8a2a8dec9614ce540b0b514e37e
Aery <KillerAery@foxmail.com>
2022-04-01 12:20:11 +0800

    更改了homework文件组织方式
    1个level做成1个类

---
+++ homework/Level1.cpp
@@ -0,0 +1,229 @@
+#include "Level1.h"
+#include <bx/uint32_t.h>
+#include "bgfx_logo.h"
+#include "imgui/imgui.h"
+
+bgfx::VertexLayout Level1::PosColorVertex::ms_layout = {};
+
+Level1::PosColorVertex Level1::s_cubeVertices[] =
+{
+	{-1.0f,  1.0f,  1.0f, 0xfff00fff },
+	{ 1.0f,  1.0f,  1.0f, 0xfff00fff },
+	{-1.0f, -1.0f,  1.0f, 0xfff00000 },
+	{ 1.0f, -1.0f,  1.0f, 0xfff00000 },
+	{-1.0f,  1.0f, -1.0f, 0x00000fff },
+	{ 1.0f,  1.0f, -1.0f, 0x00000fff },
+	{-1.0f, -1.0f, -1.0f, 0x00ffff00 },
+	{ 1.0f, -1.0f, -1.0f, 0x00ffff00 },
+};
+
+const uint16_t Level1::s_cubeTriList[] =
+{
+	0, 1, 2, // 0
+	1, 3, 2,
+	4, 6, 5, // 2
+	5, 6, 7,
+	0, 2, 4, // 4
+	4, 2, 6,
+	1, 5, 3, // 6
+	5, 7, 3,
+	0, 4, 1, // 8
+	4, 5, 1,
+	2, 3, 6, // 10
+	6, 3, 7,
+};
+
+const uint64_t Level1::s_ptState = UINT64_C(0);
+
+Level1::Level1(const char* _name, const char* _description, const char* _url)
+	: entry::AppI(_name, _description, _url)
+{
+	m_width = 0;
+	m_height = 0;
+	m_debug = BGFX_DEBUG_NONE;
+	m_reset = BGFX_RESET_NONE;
+}
+
+void Level1::init(int32_t _argc, const char* const* _argv, uint32_t _width, uint32_t _height)
+{
+	Args args(_argc, _argv);
+
+	m_width = _width;
+	m_height = _height;
+	m_debug = BGFX_DEBUG_TEXT;
+	m_reset = BGFX_RESET_VSYNC;
+
+	bgfx::Init init;
+	init.type = args.m_type;
+	init.vendorId = args.m_pciId;
+	init.resolution.width = m_width;
+	init.resolution.height = m_height;
+	init.resolution.reset = m_reset;
+	bgfx::init(init);
+
+	// Enable debug text.
+	bgfx::setDebug(m_debug);
+
+	// Set view 0 clear state.
+	bgfx::setViewClear(0
+		, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH
+		, 0x303030ff
+		, 1.0f
+		, 0
+	);
+
+	// Create vertex stream declaration.
+	PosColorVertex::init();
+
+	// Create static vertex buffer.
+	m_vbh = bgfx::createVertexBuffer(
+		// Static data can be passed with bgfx::makeRef
+		bgfx::makeRef(s_cubeVertices, sizeof(s_cubeVertices))
+		, PosColorVertex::ms_layout
+	);
+
+	// Create static index buffer for triangle list rendering.
+	m_ibh = bgfx::createIndexBuffer(
+		// Static data can be passed with bgfx::makeRef
+		bgfx::makeRef(s_cubeTriList, sizeof(s_cubeTriList))
+	);
+
+	// Create program from shaders.
+	m_program = loadProgram("vs_cubes", "fs_cubes");
+
+	m_timeOffset = bx::getHPCounter();
+
+	imguiCreate();
+}
+
+int Level1::shutdown()
+{
+	imguiDestroy();
+
+	// Cleanup.
+	bgfx::destroy(m_ibh);
+	bgfx::destroy(m_vbh);
+	bgfx::destroy(m_program);
+
+	// Shutdown bgfx.
+	bgfx::shutdown();
+
+	return 0;
+}
+
+bool Level1::update()
+{
+	if (!entry::processEvents(m_width, m_height, m_debug, m_reset, &m_mouseState))
+	{
+		imguiBeginFrame(m_mouseState.m_mx
+			, m_mouseState.m_my
+			, (m_mouseState.m_buttons[entry::MouseButton::Left] ? IMGUI_MBUT_LEFT : 0)
+			| (m_mouseState.m_buttons[entry::MouseButton::Right] ? IMGUI_MBUT_RIGHT : 0)
+			| (m_mouseState.m_buttons[entry::MouseButton::Middle] ? IMGUI_MBUT_MIDDLE : 0)
+			, m_mouseState.m_mz
+			, uint16_t(m_width)
+			, uint16_t(m_height)
+		);
+
+		showExampleDialog(this);
+
+		ImGui::SetNextWindowPos(
+			ImVec2(m_width - m_width / 5.0f - 10.0f, 10.0f)
+			, ImGuiCond_FirstUseEver
+		);
+		ImGui::SetNextWindowSize(
+			ImVec2(m_width / 5.0f, m_height / 3.5f)
+			, ImGuiCond_FirstUseEver
+		);
+		ImGui::Begin("Settings"
+			, NULL
+			, 0
+		);
+
+
+		ImGui::End();
+
+		imguiEndFrame();
+
+		// ---------------------------------- Input Events
+		// 
+		// 如果按住左键，则拖拽旋转镜头
+		if (ImGui::GetIO().MouseDown[0]) {
+			m_mousex += ImGui::GetIO().MouseDelta.x;
+			m_mousey += ImGui::GetIO().MouseDelta.y;
+		}
+		//m_mousex = bx::clamp(m_mousex,-90.0f ,90.0f );
+		m_mousey = bx::clamp(m_mousey, -90.0f, 90.0f);
+		// 滚轮缩放镜头
+		float mouseScroll = 1.0f + 0.05f * m_mouseState.m_mz;
+
+		// ---------------------------------- Logic Events
+		//
+		float time = (float)((bx::getHPCounter() - m_timeOffset) / double(bx::getHPFrequency()));
+
+		// 镜头目标
+		const bx::Vec3 at = { 0.0f, 0.0f, 0.0f };
+
+		// 镜头位置
+		float cameraRadius = 35.0f;
+		cameraRadius *= mouseScroll;
+		float horizonRadius = bx::cos(m_mousey * 0.01f);
+		const bx::Vec3 eye = {
+			cameraRadius * bx::sin(m_mousex * 0.01f) * horizonRadius,
+			cameraRadius * bx::sin(m_mousey * 0.01f),
+			cameraRadius * bx::cos(m_mousex * 0.01f) * horizonRadius
+		};
+
+		// Set view and projection matrix for view 0.
+		float view[16];
+		bx::mtxLookAt(view, eye, at);
+
+		float proj[16];
+		bx::mtxProj(proj, 60.0f, float(m_width) / float(m_height), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
+		bgfx::setViewTransform(0, view, proj);
+
+		// Set view 0 default viewport.
+		bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height));
+
+		// This dummy draw call is here to make sure that view 0 is cleared
+		// if no other draw calls are submitted to view 0.
+		bgfx::touch(0);
+
+		uint64_t state = 0
+			| BGFX_STATE_WRITE_R
+			| BGFX_STATE_WRITE_G
+			| BGFX_STATE_WRITE_B
+			| BGFX_STATE_WRITE_A
+			| BGFX_STATE_WRITE_Z
+			| BGFX_STATE_DEPTH_TEST_LESS
+			| BGFX_STATE_CULL_CW
+			| BGFX_STATE_MSAA
+			| s_ptState
+			;
+
+		// Submit 1 cubes.
+		float mtx[16];
+		bx::mtxSRT(mtx, 7, 7, 7, 0.21f, 0.37f, 0, 0, 0, 0);
+
+		// Set model matrix for rendering.
+		bgfx::setTransform(mtx);
+
+		// Set vertex and index buffer.
+		bgfx::setVertexBuffer(0, m_vbh);
+		bgfx::setIndexBuffer(m_ibh);
+
+		// Set render states.
+		bgfx::setState(state);
+
+		// Submit primitive for rendering to view 0.
+		bgfx::submit(0, m_program);
+
+		// Advance to next frame. Rendering thread will be kicked to
+		// process submitted rendering primitives.
+		bgfx::frame();
+
+		return true;
+	}
+
+	return false;
+}
---
+++ homework/Level2.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "common.h"
+#include "bgfx_utils.h"
+
+
+class Level2 : public entry::AppI {
+
+public:
+	Level2(const char* _name, const char* _description, const char* _url);
+
+	void init(int32_t _argc, const char* const* _argv, uint32_t _width, uint32_t _height) override;
+
+	virtual int shutdown() override;
+
+	bool update() override;
+
+private:
+	uint32_t m_width;
+	uint32_t m_height;
+	uint32_t m_debug;
+	uint32_t m_reset;
+
+	// 绘制相关
+	bgfx::VertexBufferHandle m_vbh;
+	bgfx::IndexBufferHandle m_ibh;
+	bgfx::ProgramHandle m_program;
+	int64_t m_timeOffset;
+
+	// 输入相关
+	entry::MouseState m_mouseState;
+	float m_mousex = 0.0f;
+	float m_mousey = 0.0f;
+
+private:
+	struct PosColorVertex
+	{
+		float m_x;
+		float m_y;
+		float m_z;
+		uint32_t m_abgr;
+		static void init()
+		{
+			ms_layout
+				.begin()
+				.add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+				.add(bgfx::Attrib::Color0, 4, bgfx::AttribType::Uint8, true)
+				.end();
+		};
+		static bgfx::VertexLayout ms_layout;
+	};
+	static PosColorVertex s_cubeVertices[];
+	static const uint16_t s_cubeTriList[];
+	static const uint64_t s_ptState;
+};
