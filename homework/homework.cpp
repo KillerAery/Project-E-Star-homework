69086ee5b31e63993d714cf398d1a157f50ede2f
Aery <KillerAery@foxmail.com>
2022-04-28 16:19:35 +0800

    Level5 初步实现shadow

--- homework/homework.cpp
+++ homework/homework.cpp
@@ -6,13 +6,15 @@
 #include "Level2.h"
 #include "Level3.h"
 #include "Level4.h"
+#include "Level5.h"
 
 int _main_(int _argc, char** _argv)
 {
-	//Level1 app("e-star-homework", "", "");
-	//Level2 app("e-star-homework", "", "");
-	//Level3 app("e-star-homework", "", "");
-	Level4 app("e-star-homework", "", "");
+	//Level1 app("e-star-homework-Level1", "", "");
+	//Level2 app("e-star-homework-Level2", "", "");
+	//Level3 app("e-star-homework-Level3", "", "");
+	//Level4 app("e-star-homework-Level4", "", "");
+	Level5 app("e-star-homework-Level5", "", "");
 	return entry::runApp(&app, _argc, _argv);
 }
 
