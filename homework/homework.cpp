ef97619e8bda8c4abdb27a19056d45e9819a8add
Aery <KillerAery@foxmail.com>
2022-05-05 09:17:55 +0800

    删除旧代码

--- homework/homework.cpp
+++ homework/homework.cpp
@@ -2,12 +2,10 @@
  * Copyright 2011-2022 Branimir Karadzic. All rights reserved.
  * License: https://github.com/bkaradzic/bgfx/blob/master/LICENSE
  */
-#include "Level5.h"
 #include "MyApp.h"
 
 int _main_(int _argc, char** _argv)
 {
-	//Level5 app("e-star-homework-Level5", "", "");
 	MyApp app("e-star-homework", "", "");
 	return entry::runApp(&app, _argc, _argv);
 }
